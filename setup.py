7dd86a2bcbee520e2973dfc06491c1f4dc0b2f46
Sebastian Horvath <sebastian.horvath@gmail.com>
2014-01-24 12:55:40 +1300

    Add setup.py and update README.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pycf',
+      version='1.0',
+      description='Python crystal field theory modules',
+      author='Sebastian Horvath',
+      author_email='sebastian.horvath@gmail.com',
+      url='https://bitbucket.org/sebastianhorvath/pycf/',
+      packages=['pycf'],
+      )
