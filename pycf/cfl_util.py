Bug fix in weighting; final sh eval.

pycf/cfl_util.py
@@ -193,7 +193,7 @@ def gen_fit_summary(coeff, fit_obj, method, fmin, **kwargs):
         del kwargs['stepsize']
  
 
-    np.set_printoptions(formatter={'float': lambda x: '{:10.4f}'.format(x)}, linewidth=140)
+    np.set_printoptions(formatter={'float': lambda x: '{:12.4f}'.format(x)}, linewidth=140)
     if kwargs['cov']:
         s += "\n" + uline_char("Covariance matrix:\n")
         try: