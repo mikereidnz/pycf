mikereidnz/pycf

commit 854c5d0d7cd77ff50fece3b9ec57dc90cb02c9f1

Correct bug in difference energy value printing; update some docstrings.

pycf/__version__.py
@@ -1,3 +1,3 @@
 
-__version__ = "0e61615"
+__version__ = "4b5cf99"
 

pycf/cfl.pyx
@@ -1024,7 +1024,9 @@ cdef class SpinHamiltonian:
 
 cdef class ExData(object):
     r"""
-    Experimental energy level data for Hamiltonians. 
+    Experimental energy level data for Hamiltonians.  If both absolute and
+    difference energy levels are both present, then ex.e will be order such that
+    all absolute energy level values are before the difference energy levels. 
 
     Parameters
     ----------
@@ -1194,6 +1196,8 @@ cdef class ExData(object):
                     self.fldh = np.ascontiguousarray(fldh, dtype=np.int32)
                     
                 if len(key) == 2:
+                    # Both abs. and diff. levels present; energies are stacked
+                    # with all abs. values before the diff. values. 
                     self.e = np.ascontiguousarray(np.hstack((data[key.index('AS')][:, ll],
                         data[key.index('DS')][:, 2*ll])), dtype=np.float64)
                 elif key[0] == 'AS':
@@ -1222,6 +1226,8 @@ cdef class ExData(object):
                     self.ild = np.ascontiguousarray(data[key.index('D')][:, 0]-1, dtype=np.int32)
                     self.fld = np.ascontiguousarray(data[key.index('D')][:, 1]-1, dtype=np.int32)
                 if len(key) == 2:
+                    # Both abs. and diff. levels present; energies are stacked
+                    # with all abs. values before the diff. values. 
                     self.e = np.ascontiguousarray(np.hstack((data[key.index('A')][:, 1],
                         data[key.index('D')][:, 2])), dtype=np.float64)
                 elif key[0] == 'A':

pycf/cfl_util.py
@@ -176,6 +176,7 @@ def ex_parse_abs(ex, z, labels):
     
     else:
         parsed_ex = np.zeros((ex.n_a, 2))
+        # Abs. energy values are ordered to preceed diff. values.
         parsed_ex[:, 1] = ex.e[:ex.n_a]
         parsed_ex[:, 0] = ex.la 
         # Sort ex according to index column.
@@ -204,10 +205,10 @@ def ex_parse_diff(ex, z, labels):
         Three coloumn array containing initial level indices starting at 1 in
         the zeroeth column, final level indices starting at 1 in the first
         column, and corresponding experimental energy levels differences in the
-        second column.  If the ExData object contains no absolute energy levels
+        second column.  If the ExData object contains no difference energy levels
         None is returned.
     """
-    if ex.n_a == 0:
+    if ex.n_d == 0:
         parsed_ex = None
     elif ex.sl_index:
         parsed_ex = np.zeros((ex.n_d, 3))
@@ -222,6 +223,7 @@ def ex_parse_diff(ex, z, labels):
             parsed_ex[i, 1] = np.where((np.array(labels)[pc] == s).all(axis=1))[0][0]
     else:
         parsed_ex = np.zeros((ex.n_d, 3))
+        # Diff. energy values are ordered to come after abs. values.
         parsed_ex[:, 2] = ex.e[ex.n_a:]
         parsed_ex[:, 0] = ex.ild
         parsed_ex[:, 1] = ex.fld

commit b2241e08b4c5599b1c4c1f38f46b8404bd568894

Ensure coeff stored by Runner object is a deep copy; otherwise 854c5d0 broke initial energy level summary printing.

pycf/__version__.py
@@ -1,3 +1,3 @@
 
-__version__ = "854c5d0"
+__version__ = "478615a"
 

pycf/cfl.pyx
@@ -1405,8 +1405,8 @@ cdef class EFitRunner(object):
         if h.coeff_dict == None:
             raise ValueError("Hamiltonian must have coefficients set prior to efit.")
         else:
-            self.coeff = h.coeff_dict
-        
+            self.coeff = copy.deepcopy(h.coeff_dict)
+
         self.param_types = {}
         self.n_p_real = 0
         for p in parameters:
@@ -1623,7 +1623,7 @@ cdef class MHFitRunner(object):
             if h.coeff_dict == None:
                 raise ValueError("Hamiltonian must have coefficients set prior to mhfit.")
             else:
-                self.coeff.update(h.coeff_dict)
+                self.coeff.update(copy.deepcopy(h.coeff_dict))
             h_param_list += [[p for p in parameters if p in h]]
             self.n_zx[i] = len(h_param_list[i])
         
@@ -1984,7 +1984,7 @@ cdef class ESHFitRunner(object):
         if h.coeff_dict == None:
             raise ValueError("Hamiltonian must have coefficients set prior to eshfit.")
         else:
-            self.coeff = h.coeff_dict
+            self.coeff = copy.deepcopy(h.coeff_dict)
 
         if not sh.pro_data_set:
             raise ValueError("Spin Hamiltonian must have projection data set prior to eshfit.")
@@ -2287,7 +2287,7 @@ cdef class MESHFitRunner(object):
             if h.coeff_dict == None:
                 raise ValueError("Hamiltonian must have coefficients set prior to meshfit.") 
             else:
-                self.coeff.update(h.coeff_dict)
+                self.coeff.update(copy.deepcopy(h.coeff_dict))
             
             h_param_list += [[p for p in parameters if p in h]]
             n_zxa[i] += len(h_param_list[i])