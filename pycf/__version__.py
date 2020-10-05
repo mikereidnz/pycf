mikereidnz/pycf

commit ff1d045aae7c5d83767471be21a959c8c6216621

Fix corner case bug introduced into mhfit summary printing by last commit.

pycf/__version__.py
@@ -1,3 +1,3 @@
 
-__version__ = "b'e4325c7'"
+__version__ = "b'426f68c'"
 

pycf/cfl.pyx
@@ -3376,7 +3376,7 @@ def mh_fit(parameters, h_list, weights_list, ex_list, cfl_min, **kwargs):
         
         name = "Hamiltonian %i" % i
         summary += gen_e_summary_trunc(h.w, h.z, h.tensors[0].states.labels, h.tensors[0].states.label_key,
-                ex=ex_list[i], name=name, chi2=mhfit.chi2[i], ndof=ndof, weighting=mhfit.weights_list[i])
+                ex=mhfit.ex_list[i], name=name, chi2=mhfit.chi2[i], ndof=ndof, weighting=mhfit.weights_list[i])
 
         summary += "\n"
 

commit 427fc70c439153e5df659f584a69d7c0144f30d0

Fix corner-case crash introduced by ff1d045 summary printing fix.

pycf/__version__.py
@@ -1,3 +1,3 @@
 
-__version__ = "b'ec146f9\n'"
+__version__ = "b'7013025\n'"
 

pycf/cfl.pyx
@@ -2437,9 +2437,9 @@ cdef class MESHFit(object):
             if 'ex' in d:
                 ex = d['ex']
                 if not isinstance(ex, ExData):
-                    ex_list += [ExData(ex)]
-                else:
-                    ex_list += [ex]
+                    ex = ExData(ex)
+                
+                ex_list += [ex]
                 self.n_obs += ex_list[i].n_obs
                 n_ex += 1
                 if ex.sl_index: